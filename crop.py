6ad182c9f0fb83ccb786bcad0f0f91d123cab7cd
Mauro Oliveira <Mauro.Oliveira@dell.com>
2022-03-13 20:13:57 -0300

    improvements

--- crop.py
+++ crop.py
@@ -3,11 +3,13 @@
 from PIL import Image
 import os
 
-img_folder = "C:\\Users\\Mauro_Oliveira\\Pictures\\Screenshots"
-output_folder = "C:\\Users\\Mauro_Oliveira\\Pictures\\Screenshots\\cropped"
-file_extension = ".png"
 
-def crop():
+def main():
+    img_folder = "C:\\Users\\Mauro_Oliveira\\Pictures\\Screenshots"
+    output_folder = "C:\\Users\\Mauro_Oliveira\\Pictures\\Screenshots\\cropped"
+    file_extension = ".png"
+    resolution_box = (0, 0, 1920, 1080)
+
     for file_name in os.listdir(img_folder):
         file_path = os.path.join(img_folder, file_name)
         output_file_path = os.path.join(output_folder, file_name)
@@ -20,9 +22,9 @@ def crop():
             continue
 
         with Image.open(file_path) as image:
-            imCrop = image.crop((0, 0, 1920, 1080))
+            imCrop = image.crop(resolution_box)
             imCrop.save(output_file_path, "BMP", quality=100)
             print(f"File {file_name} successfully croped!")
 
 if __name__ == "__main__":
-    crop()
+    main()
